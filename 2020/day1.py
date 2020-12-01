5fc89c1372c90e6a49201f8d81b705cdb161f888
AlrikRr <32389491+AlrikRr@users.noreply.github.com>
2020-12-01 16:32:32 +0100

    Add comments

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
+# Array used to store the numbers
 nums = []
 
 with open("day1/puzzle.txt",'r') as f:
     for line in f:
+            # Put each numbers into the nums array, don't forget rstrip ! 
             nums.append(line.rstrip("\n"))
 
+# So here I decided to use the simple way.
+# There are 200 numbers, I get the sum of the 1st element of x with the 1st element of y
+# Then the Sum of the 1st element of x and the 2nd element of y ......
+# Until a result returns 2020. Not the fastest way but it works !
 for i in range(200):
     for y in range(200):
-        #print(str(nums[i])+" + "+str(nums[y]))
         res = int(nums[i]) + int(nums[y])
-        #print(res)
         if res == 2020:
             print("Finish : "+str(nums[i])+" + "+str(nums[y])+" = "+str(res))
             key = int(nums[i]) * int(nums[y])
